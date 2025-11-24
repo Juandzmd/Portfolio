51b6221284f7e04e9815b57eada29365f7874931
Juandzm <juandzmd@gmail.com>
2025-11-24 15:14:27 -0300

    chore: add assetPrefix to /Portfolio

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,7 @@ const nextConfig: NextConfig = {
   // IMPORTANT: If deploying to a user page (username.github.io), leave basePath empty.
   // If deploying to a project page (username.github.io/repo-name), uncomment and set the repo name.
   basePath: '/Portfolio',
+  assetPrefix: '/Portfolio',
 };
 
 export default nextConfig;
